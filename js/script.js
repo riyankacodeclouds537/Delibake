10fa23afbe53a68f22a36e731d98c3d6b35f6467
riyankacodeclouds537 <riyanka.nag@codeclouds.co.in>
2025-06-10 15:49:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    mobileMenuButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+  });
+  const cards = document.querySelectorAll('.category-card');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('opacity-100', 'translate-y-0');
+        entry.target.classList.remove('opacity-0', 'translate-y-6');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.1,
+  });
+
+  cards.forEach(card => {
+    observer.observe(card);
+  });
+  
